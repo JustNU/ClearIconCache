d51be92c3daeb9b9c006d4583a59ef680336eee3
JustNU <ivancool108@gmail.com>
2023-09-25 06:51:02 +0300

    actual code

--- Plugin.cs
+++ Plugin.cs
@@ -1,33 +1,27 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using System;
 using System.IO;
 using UnityEngine;
 
 
-namespace IconResolutionMult
+namespace ClearIconCache
 {
-    [BepInPlugin("com.JustNU.IconResolutionMult", "JustNU-IconResolutionMult", "1.0.0")]
+    [BepInPlugin("com.JustNU.ClearIconCache", "JustNU-ClearIconCache", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
         // create config ent
-        public static ConfigEntry<int> ConfItemIconResMult;
-        public static ConfigEntry<int> ConfClothingIconResMult;
-        public static ConfigEntry<bool> ConfCleanIconCache;
-        public static ConfigEntry<bool> ConfModFuncEnabled;
+        public static ConfigEntry<bool> ConfClearIconCache;
 
         private void Awake()
         {
             // set up config
-            ConfCleanIconCache = Config.Bind("Core", "Clean Icon Cache on start-up", false, "Cleans icon cache");
-            ConfItemIconResMult = Config.Bind("Values", "Item Icon Resolution Multiplier", 1, new ConfigDescription("Set multiplier for item icon's png resolution", new AcceptableValueRange<int>(1, 8)));
-            ConfClothingIconResMult = Config.Bind("Values", "Clothing Resolution Multiplier", 1, new ConfigDescription("Set multiplier for clothing icon's png resolution", new AcceptableValueRange<int>(1, 20)));
+            ConfClearIconCache = Config.Bind("Core", "Clears Icon Cache on game start-up", false, "Clears icon cache");
 
             // Plugin startup logic
-            Logger.LogInfo($"Plugin com.JustNU.IconResolutionMult is loading");
+            Logger.LogInfo($"Plugin com.JustNU.ClearIconCache is loading");
 
             // clean icon cache
-            if (Plugin.ConfCleanIconCache.Value == true)
+            if (Plugin.ConfClearIconCache.Value == true)
             {
                 System.IO.DirectoryInfo IconCachePath = new DirectoryInfo(Path.Combine(Application.temporaryCachePath, "Icon Cache\\live"));
                 System.IO.DirectoryInfo ClothingCachePath = new DirectoryInfo(Path.Combine(Application.temporaryCachePath, "Icon Cache\\live\\Clothing"));
@@ -48,9 +42,6 @@ namespace IconResolutionMult
                     }
                 }
             }
-
-            new itemIconPatch().Enable();
-            //new ClothingIconPatch().Enable();
         }
     }
 }
